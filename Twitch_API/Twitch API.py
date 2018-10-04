37985b8d77cd444c115d379659aea7a175dc7b4a
matthewcoleka <41554405+matthewcoleka@users.noreply.github.com>
2018-10-04 19:44:01 -0400

    Matt Cole push
    Wrapped code in a loop to pull every hour for ten days

--- Twitch_API/Twitch API.py
+++ Twitch_API/Twitch API.py
@@ -22,52 +22,57 @@ topgames_base_url =f"https://api.twitch.tv/kraken/games/top/?client_id={client_I
 
 
 # In[42]:
+loops = np.arange(0,168,1)
+for loop in loops:
+    timestamps = []
+    game_names = []
+    game_ranks = []
+    game_giantbombs = []
+    game_viewers = []
+    game_channels = []
+
+    topgames_data = requests.get(topgames_base_url).json()
+
+    for i in range(0,25):
+
+        game_name = topgames_data['top'][i]['game']['name']
+        game_names.append(game_name)
+        game_ranks.append(i + 1)
+
+        game_giantbomb = topgames_data['top'][i]['game']['giantbomb_id']
+        game_giantbombs.append(game_giantbomb)
+
+        game_tot_viewers = topgames_data['top'][i]['viewers']
+        game_viewers.append(game_tot_viewers)
+
+        game_tot_channels = topgames_data['top'][i]['channels']
+        game_channels.append(game_tot_channels)
+
+        unix_time = time.time()
+        timestamp = datetime.datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M')
+        timestamps.append(timestamp)
 
 
-timestamps = []
-game_names = []
-game_ranks = []
-game_giantbombs = []
-game_viewers = []
-game_channels = []
 
-topgames_data = requests.get(topgames_base_url).json()
-
-for i in range(0,25):
-    
-    game_name = topgames_data['top'][i]['game']['name']
-    game_names.append(game_name)
-    game_ranks.append(i + 1)
-        
-    game_giantbomb = topgames_data['top'][i]['game']['giantbomb_id']
-    game_giantbombs.append(game_giantbomb)
-        
-    game_tot_viewers = topgames_data['top'][i]['viewers']
-    game_viewers.append(game_tot_viewers)
-        
-    game_tot_channels = topgames_data['top'][i]['channels']
-    game_channels.append(game_tot_channels)
-        
-    unix_time = time.time()
-    timestamp = datetime.datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M')
-    timestamps.append(timestamp)
-    
+    # In[44]:
 
 
-# In[44]:
+    top_games_df = pd.DataFrame({"Timestamp": timestamps,
+                                 "Game Rank": game_ranks,
+                                 "Game": game_names,
+                                 "Giantbomb ID": game_giantbombs,
+                                 "Total Viewers": game_viewers,
+                                 "Total Channels": game_channels})
+    top_games_df
 
 
-top_games_df = pd.DataFrame({"Timestamp": timestamps,
-                             "Game Rank": game_ranks,
-                             "Game": game_names,
-                             "Giantbomb ID": game_giantbombs,
-                             "Total Viewers": game_viewers,
-                             "Total Channels": game_channels})
-top_games_df
+    # In[47]:
 
 
-# In[47]:
-
-
-top_games_df.to_csv(f"game {timestamps[0]}.csv", index = False)
+    top_games_df.to_csv(f"game {timestamps[0]}.csv", index = False)
+    #Print loop number
+    print(f'On loop {loop}')
+    print('----------------')
+    #Sleep 3600 seconds/1 hour
+    timer.sleep(3600)
 
